67826eaca43d71a73e13222391d8c36e1acf37b0
Wender Barbosa <wenblack012@gmail.com>
2022-09-13 01:42:24 -0300

    Create folders and config TypeScript

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (request, response)=>{
+  return response.json([
+    {id:1 , name: 'Advertising 1'},
+    {id:2 , name: 'Advertising 2'},
+    {id:3 , name: 'Advertising 3'}
+  ])
+})
+
+
+app.listen(3333)
