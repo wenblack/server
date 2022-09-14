2faabbc9e4304d2fd421daee600b3ba85731e4b8
Wender Barbosa <wenblack012@gmail.com>
2022-09-13 21:32:56 -0300

    Create base to server api

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,8 @@ app.get('/ads', (request, response)=>{
   return response.json([
     {id:1 , name: 'Advertising 1'},
     {id:2 , name: 'Advertising 2'},
-    {id:3 , name: 'Advertising 3'}
+    {id:3 , name: 'Advertising 3'},
+    {id:4 , name: 'Advertising 4'}
   ])
 })
 
