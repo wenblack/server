8804807cdb73ed15a472f7fcbcddb98c213d9a97
Wender Barbosa <wenblack012@gmail.com>
2022-09-18 13:58:01 -0300

    Create routes for API

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response)=>{
-  return response.json([
-    {id:1 , name: 'Advertising 1'},
-    {id:2 , name: 'Advertising 2'},
-    {id:3 , name: 'Advertising 3'},
-    {id:4 , name: 'Advertising 4'}
-  ])
+app.get('/games', (request, response) =>{
+  return response.json([])
 })
 
+app.post('/ads', (request, response) =>{
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response)=>{
+  const gameId = request.params.id
+
+  return response.send(gameId);
+})
+
+
+app.get('/ads/:id/discord', (request, response)=>{
+  const adId = request.params.id
+  return response.json([])
+})
 
 app.listen(3333)
